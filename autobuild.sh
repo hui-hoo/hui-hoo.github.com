c1b68b9e0028903bbcfd8cc4a8975fd84b5942d5
huihoo <hui_hoo@163.com>
2016-01-07 22:58:09 +0800

    fix typo

--- autobuild.sh
+++ autobuild.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-[ -z "$1" ] && echo "Error: $1 should be a commit message" && exit 127
+[ -z "$1" ] && echo "Error: \$1 should be a commit message" && exit 127
 
-jekyll build 
 
 git add .
 git commit -m "$1"
